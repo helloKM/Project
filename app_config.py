fd452ff9c5228e0629d27e941e8279a0e8d23c5a
haha370104 <137368440@qq.com>
2016-03-30 14:46:45 +0800

    Delete app_config.py

--- app_config.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Ab123456@115.28.206.58:3306/Project'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
